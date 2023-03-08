503f056942dda61437e8464f690719bbdc214d37
just4danish@gmail.com <just4danish@gmail.com>
2023-03-08 16:38:29 +0400

    cron issue fixed

--- entity/jobs/scheduler.py
+++ entity/jobs/scheduler.py
@@ -3,6 +3,5 @@ from .job import update_entity_grease_trap_cleaning_status
 
 def start():
 	scheduler = BackgroundScheduler(timezone="Asia/Dubai")
-	# scheduler.add_job(update_entity_grease_trap_cleaning_status, 'cron', hour=0, minute=0, second=0)
-	scheduler.add_job(update_entity_grease_trap_cleaning_status, 'cron', hour=16, minute=36, second=0)
+	scheduler.add_job(update_entity_grease_trap_cleaning_status, 'cron', hour=0, minute=0, second=0)
 	scheduler.start()
